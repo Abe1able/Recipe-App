fc4492e26c4b536bb20473f89e2374024c827fba
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-05 18:48:54 +0500

    Displaying Food List

---
+++ app/models/food.rb
@@ -0,0 +1,9 @@
+class Food < ApplicationRecord
+    belongs_to :user, class_name: 'User'
+    has_many :recipe_foods
+
+    validates :name, presence: true
+    validates :measurement_unit, presence: true
+    validates :price, presence: true
+    validates :quantity, presence: true
+end
