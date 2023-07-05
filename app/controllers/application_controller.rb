e67e75ad4d0f97cd931688faf9df7ba7d8864d40
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-05 21:24:24 +0500

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.first
   end
 
-
   protect_from_forgery with: :exception
-  
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
