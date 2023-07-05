868e02b68d6962721c15a5b4d108bba57bbc70a3
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-05 20:25:23 +0500

    updated controllers methods

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,12 +1,30 @@
 class FoodsController < ApplicationController
    def index
+    @foods=Food.all.includes([:user])
    end
    def show
    end
    def new
+    @new=Food.new
    end
+
+
    def create
+    @food=Food.new(params.require(:foods).permit(:name, :measurement_unit, :price, :quantity))
+    @food.user_id=current_user.id
+    if @food.save
+        redirect_to foods_path
+        flash[:notice]="food saved successfully"
+    else
+        render :new
+        flash[:notice]="food has not been saved"
+    end
    end
+
+
    def destroy 
+    @food=Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
    end
 end
