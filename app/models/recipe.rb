909f63fe9949dba449400dae2e3a9996d8f91afd
Abe1able <abel.workmail@gmail.com>
2023-07-06 03:24:42 +0300

    Fix linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
-	has_many :recipe_foods, dependent: :destroy
-	belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  belongs_to :user
 
-	validates :name, presence: true
-	validates :cooking_time, presence: true
-	validates :preparation_time, presence: true
-	validates :description, presence: true
+  validates :name, presence: true
+  validates :cooking_time, presence: true
+  validates :preparation_time, presence: true
+  validates :description, presence: true
 end
